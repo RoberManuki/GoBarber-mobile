6c93f61d40b56cd55e936e7bb0cf7e5cddee848c
MaNuki <robercraudio@gmail.com>
2020-11-27 01:56:10 -0300

    Inicial commit

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from './styles'
+
+const SignUp: React.FunctionComponent = () => {
+  return (
+    <>
+      <Container />
+    </>
+  )
+}
+
+export default SignUp;
